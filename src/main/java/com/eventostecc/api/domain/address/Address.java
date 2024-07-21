0cc1c1b09aed6493df1bb04d66f210eaeeb2242c
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-07-21 13:25:52 -0300

    :tada: Initial Commit

---
+++ src/main/java/com/eventostecc/api/domain/address/Address.java
@@ -0,0 +1,39 @@
+package com.eventostecc.api.domain.address;
+
+import java.util.UUID;
+
+import com.eventostecc.api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+	@Id
+	@GeneratedValue
+	private UUID id;
+	
+	private String city;
+	private String uf;
+	
+	@ManyToOne
+	@JoinColumn(name = "event_id")
+	private Event event;
+	
+	
+	
+	
+}
---
+++ src/test/java/com/eventostecc/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventostecc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
