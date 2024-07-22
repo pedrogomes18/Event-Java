b063bd0afe8b4610898cdde1b928e674bffad52e
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-07-21 22:32:33 -0300

    :bug: fix: Event Response DTO

--- src/main/java/com/eventostecc/api/domain/event/EventResponseDTO.java
+++ src/main/java/com/eventostecc/api/domain/event/EventResponseDTO.java
@@ -1,14 +1,7 @@
 package com.eventostecc.api.domain.event;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.util.Date;
 import java.util.UUID;
 
-public record EventResponseDTO(UUID id, String title,
-                               String description, Date date, String city, String uf, Boolean remote,
-                               String eventUrl, String imageUrl) {
-
-    public EventResponseDTO(UUID id, String title, String description, Date date, Boolean remote, String eventUrl, String imgUrl) {
-    }
+public record EventResponseDTO(UUID id, String title, String description, Date date, Boolean remote, String eventUrl, String imgUrl) {
 }
