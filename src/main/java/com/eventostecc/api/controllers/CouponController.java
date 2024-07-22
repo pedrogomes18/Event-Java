860aee3c9cbacbaba5ddb471db0bb08d58276cbf
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-07-21 22:03:02 -0300

    :heavy_plus_sign: feat: Add Coupon To Event

---
+++ src/main/java/com/eventostecc/api/controllers/CouponController.java
@@ -0,0 +1,24 @@
+package com.eventostecc.api.controllers;
+
+import com.eventostecc.api.domain.coupon.Coupon;
+import com.eventostecc.api.domain.coupon.CouponRequestDTO;
+import com.eventostecc.api.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/coupon")
+public class CouponController {
+
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping("/event/{eventId}")
+    public ResponseEntity<Coupon> addCouponsToEvent(@PathVariable UUID eventId, @RequestBody CouponRequestDTO data) {
+        Coupon coupons = couponService.addCouponToEvent(eventId, data);
+        return ResponseEntity.ok(coupons);
+    }
+}
